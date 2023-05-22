30885efb8863ad31a04c75a8067b41b4c1ecab88
Timur2009KG <asanalievt93@gmail.com>
2023-05-22 21:18:18 +0300

    first commit

---
+++ lesson_4_homework.dart
@@ -0,0 +1,91 @@
+void main() {
+  // dynamic a = "1";
+  // num a = 1;
+  // dynamic a = 3;
+
+  // if (a == ){
+  //   print("verno");
+  // }else{
+  //   print("error");
+  // }
+  
+  
+  // String a = "test";
+  // String a = "Test";
+  // dynamic a = 3;
+
+  // if (a == "test"){
+  //   print("right");
+  // }else{
+  //   print("error");
+  // }
+  
+  //int a = 1;
+  // int a = 0;
+  //  int a = -3;
+
+  // if (a != 0) {
+  //   print("йухху");
+  // } else {
+  //   print("error");
+  // }
+  
+  // int a = 1;
+  // int a = 0;
+  //  int a = -3;
+
+  // if (a <= 0) {
+  //   print("йухху");
+  // } else {
+  //   print("error");
+  // }
+
+//   int a = 1;
+//   // int a = 0;
+//   // int a = -3;
+
+//  if (a >= 0) {
+//    print("йухху");
+//  } else {
+//    print("error");
+//  }
+  
+  
+  // int a = 1;
+  // // int a = 0;
+  // // int a = -3;
+
+  // if (a < 0) {
+  //   print("йухху");
+  // } else {
+  //   print("error");
+  // }
+
+  // int a = 1;
+  // int a = 0;
+  //  int a = -3;
+
+  // if (a > 0) {
+  //   print("йухху");
+  // } else {
+  //   print("error");
+  // }
+
+  // int a = 1;
+  // int a = 0;
+  // int a = -3;
+
+  // if (a == 0) {
+  //   print("верно");
+  // } else if (a > 0 || a < 0) {
+  //   print("неверно");
+  // }
+
+  // int a = 10;
+
+  // if ( a > 10){
+  //   print('неверно');
+  // }else{
+  //   print("верно");
+  // }
+}
